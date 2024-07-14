6d4b9695553c4e26ac162bb270135918399d0557
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-07-13 17:11:44 -0700

    Runtime: 0 ms (100.00%), Memory: 12.9 MB (46.90%) - Gitcode

---
+++ 39-combination-sum/39. Combination Sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    vector<vector<int>> ans;
+    void solve(vector<int>& candidates, int target, int i, vector<int> & curr){
+        if(target<0 or i>=candidates.size())return;
+        if(target==0){
+            ans.push_back(curr);
+            return;
+        }
+        curr.push_back(candidates[i]);
+        solve(candidates,target-candidates[i],i,curr);
+        curr.pop_back();
+        solve(candidates,target,i+1,curr);
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> curr;
+        sort(candidates.begin(),candidates.end());
+        solve(candidates,target,0, curr);
+        return ans;
+    }
+};
