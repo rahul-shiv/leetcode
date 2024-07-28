c79d2f82850eb041c16d09f030b35263ae56fe24
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-07-28 06:44:39 -0700

    Runtime: 879 ms (12.46%), Memory: 104 MB (5.04%) - Gitcode

---
+++ 128-longest-consecutive-sequence/128. Longest Consecutive Sequence.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    map<int,int> m;
+    map<int,int> sz;
+    int find(int x){
+        if(x==m[x])return x;
+        return m[x]=find(m[x]);
+    }
+    int union_(int x, int y){
+        int a = find(x);
+        int b = find(y);
+        if(a==b)return sz[a];
+        if(sz[a]>sz[b]){
+            swap(x,y);
+            swap(a,b);
+        }
+        sz[b]+=sz[a];
+        m[a]=b;
+        find(x);
+        return sz[b];
+    }
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int ans = !nums.empty();
+        for(auto num:nums){
+            auto it = m.find(num);
+            if(it==m.end()){
+                m[num]=num;
+                sz[num]=1;
+            }
+            it = m.find(num-1);
+            if(it!=m.end()){
+                ans = max(ans,union_(num,num-1));
+            }
+            it = m.find(num+1);
+            if(it!=m.end()){
+                ans = max(ans,union_(num,num+1));
+            }
+        }
+        return ans;
+    }
+};
