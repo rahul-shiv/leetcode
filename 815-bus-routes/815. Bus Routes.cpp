48e479d61244ba5661193010778e9356d44b93c4
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-07-27 18:47:47 -0700

    Runtime: 406 ms (46.07%), Memory: 62.6 MB (63.57%) - Gitcode

--- 815-bus-routes/815. Bus Routes.cpp
+++ 815-bus-routes/815. Bus Routes.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
         if(source==target)return 0;
-        int i = 0;
         unordered_map<int,vector<int>> g;
-        unordered_set<int> vis;
-        for(auto &route:routes){
-            for(auto x:route){
-                g[x].push_back(i);
+        vector<bool> vis(routes.size());
+        for(int i = 0; i < routes.size();i++){
+            for(auto u:routes[i]){
+                g[u].push_back(i);
             }
-            i++;
         }
         queue<pair<int,int>> q;
-        int u,dep;
         q.push({source,0});
+        int u,d;
         while(!q.empty()){
-            tie(u,dep)=q.front();
+            tie(u,d)=q.front();
             q.pop();
             for(auto route:g[u]){
-                auto it = vis.find(route);
-                if(it!=vis.end())continue;
-                vis.insert(route);
+                if(vis[route])continue;
+                vis[route]=true;
                 for(auto v:routes[route]){
-                    if(v==target)return dep+1;
-                    q.push({v,dep+1});
+                    if(v==target)return d+1;
+                    q.push({v,d+1});
                 }
             }
         }
