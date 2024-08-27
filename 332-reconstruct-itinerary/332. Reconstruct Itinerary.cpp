3023cb1424612aff0cafe5db2228b3e581b4903a
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-08-26 23:49:10 -0700

    Runtime: 14 ms (81.46%), Memory: 21.3 MB (9.63%) - Gitcode

---
+++ 332-reconstruct-itinerary/332. Reconstruct Itinerary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    vector<string> ans;
+    unordered_map<string,multiset<string>> m;
+    int n;
+public:
+    bool solve(string curr){
+        if(ans.size()==n){
+            return true;
+        }
+        bool t = false, temp;
+        auto it = m[curr].begin();
+        while(it!=m[curr].end()){
+            auto next = *it;
+            m[curr].erase(it);
+            ans.push_back(next);
+            t = solve(next);
+            if(t)break;
+            ans.pop_back();
+            it = m[curr].insert(next);
+            it++;
+        }
+        return t;
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        n = tickets.size()+1;
+        for(auto ticket:tickets){
+            m[ticket[0]].insert(ticket[1]);
+        }
+        ans.push_back("JFK");
+        solve("JFK");
+        return ans;
+    }
+};
