f433e9b122b627ea8870fabc5d56d44028076ade
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-09-07 21:51:11 -0700

    Runtime: 16 ms (48.95%), Memory: 20.4 MB (32.00%) - Gitcode

--- 135-candy/135. Candy.cpp
+++ 135-candy/135. Candy.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        int n = ratings.size(), ans = 0;
-        if(n==1)return 1;
+    int candy(vector<int>& nums) {
+        int n = nums.size(), ans = 0;
         vector<int> v(n,1);
-        vector<pair<int,int>> arr;
-        for(int i = 0; i< n; i++){
-            arr.push_back({ratings[i],i});
+        for(int i = 1; i< n; i++){
+            if(nums[i]>nums[i-1]){
+                v[i]=v[i-1]+1;
+            }
         }
-        sort(arr.begin(),arr.end());
-        for(auto &x:arr){
-            if(x.second and ratings[x.second]>ratings[x.second-1]){
-                v[x.second] = v[x.second-1]+1;
+        ans = v[n-1];
+        for(int i = n-2; i>=0; i--){
+            if(nums[i]>nums[i+1]){
+                v[i]=max(v[i],v[i+1]+1);
             }
-            if(x.second<n-1 and ratings[x.second]>ratings[x.second+1]){
-                v[x.second] = max(v[x.second],v[x.second+1]+1);
-            }
-            ans+=v[x.second];
+            ans+=v[i];
         }
         return ans;
     }
