7122a9e7acf7bd2c026dbc596d7ae0e7a6f05278
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-09-07 17:20:55 -0700

    Runtime: 12 ms (55.38%), Memory: 19.9 MB (21.26%) - Gitcode

--- 45-jump-game-ii/45. Jump Game II.cpp
+++ 45-jump-game-ii/45. Jump Game II.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        queue<pair<int,int>> q;
         int n = nums.size();
-        vector<int>vis(n,INT_MAX); 
-        vis[0]=0;
-        int m = 1;
-        for(int i = 0; i < n; i++){
-            for(; m<n and m<=i+nums[i];m++){
-                vis[m]=vis[i]+1;
+        vector<int> dp(n,1e4+1);
+        dp[0]=0;
+        int sf=1;
+        for(int i = 0;i<n;i++){
+            for(;sf<min(n,i+nums[i]+1);sf++){
+                dp[sf]=dp[i]+1;
             }
-            if(n==m)break;
         }
-        
-        return vis[n-1];
+        return dp[n-1];
     }
 };
