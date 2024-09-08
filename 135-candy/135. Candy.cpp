15b273ec11335e5514ca959703f57b30275489d5
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-09-07 21:45:49 -0700

    Runtime: 34 ms (8.60%), Memory: 24.1 MB (5.13%) - Gitcode

--- 135-candy/135. Candy.cpp
+++ 135-candy/135. Candy.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    int candy(vector<int>& nums) {
-        int n = nums.size(), p = 0;
-        vector<int> vals(n);
-        vals[0]=1;
-        for(int i = 1; i < n; i++){
-            if(nums[i]>nums[i-1]){
-                p = i;
-                vals[i]=vals[i-1]+1;
-            }else if(nums[i]==nums[i-1]){
-                p = i;
-                vals[i]=1;
-            }else{
-                while(i+1<n and nums[i+1]<nums[i])i+=1;
-                vals[i]=1;
-                for(int j = i-1; j >= p ; j--){
-                    if(vals[j]<=vals[j+1])vals[j]=vals[j+1]+1;
-                }
-            }
+    int candy(vector<int>& ratings) {
+        int n = ratings.size(), ans = 0;
+        if(n==1)return 1;
+        vector<int> v(n,1);
+        priority_queue<pair<int,int>> pq;
+        for(int i = 0; i< n; i++){
+            pq.push({-ratings[i],i});
         }
-        return accumulate(vals.begin(),vals.end(),0);   
+        pair<int,int> x;
+        while(!pq.empty()){
+            x = move(pq.top());
+            pq.pop();
+            if(x.second and ratings[x.second]>ratings[x.second-1]){
+                v[x.second] = v[x.second-1]+1;
+            }
+            if(x.second<n-1 and ratings[x.second]>ratings[x.second+1]){
+                v[x.second] = max(v[x.second],v[x.second+1]+1);
+            }
+            ans+=v[x.second];
+        }
+        return ans;
     }
 };
-
-// 1 2 1 2 1
-
-// 1 2 3 1 3 2 1 
