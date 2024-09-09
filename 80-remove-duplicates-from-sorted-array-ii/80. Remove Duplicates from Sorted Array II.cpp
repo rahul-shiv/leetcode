0ed1055cc8e2497c81c195d6aef266b6b47c11d3
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-09-08 18:08:14 -0700

    Runtime: 0 ms (100.00%), Memory: 13.7 MB (25.91%) - Gitcode

--- 80-remove-duplicates-from-sorted-array-ii/80. Remove Duplicates from Sorted Array II.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80. Remove Duplicates from Sorted Array II.cpp
@@ -3,15 +3,11 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int l = 0, r = 1;
         while(r<nums.size()){
-            if(nums[r]==nums[l]){
-                nums[l+1]=nums[r];
-                l++;
-                r++;
+            nums[l+1]=nums[r];
+            l++;
+            r++;
+            if(nums[r-1]==nums[l-1]){
                 while(r<nums.size() and nums[r]==nums[l])r++;
-            }else{
-                nums[l+1]=nums[r];
-                l++;
-                r++;
             }
         }
         return l+1;
