1e5ca2aeb925e74a26de4da8ece8e53213873f96
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-09-16 23:24:58 -0700

    Runtime: 0 ms (100.00%), Memory: 8.9 MB (45.28%) - Gitcode

---
+++ 1114-print-in-order/1114. Print in Order.cpp
@@ -0,0 +1,35 @@
+class Foo {
+    condition_variable cv;
+    mutex m;
+    int x;
+public:
+    Foo() {
+        x = 1;
+    }
+
+    void first(function<void()> printFirst) {
+        std::unique_lock lock(m);
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        x=2;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        std::unique_lock lock(m);
+        cv.wait(lock, [&]{return x==2;});
+        
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        x=3;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        std::unique_lock lock(m);
+        cv.wait(lock, [&]{return x==3;});
+        
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
