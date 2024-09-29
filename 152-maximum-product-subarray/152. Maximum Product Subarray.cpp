5be4eba7a528ae22400fceb557f969e5b389eaa3
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-09-29 11:05:48 -0700

    Runtime: 5 ms (60.52%), Memory: 16.2 MB (100.00%) - Gitcode

--- 152-maximum-product-subarray/152. Maximum Product Subarray.cpp
+++ 152-maximum-product-subarray/152. Maximum Product Subarray.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        double ma=nums[0],mi=nums[0];
-        double ans = ma;
-        for(int i=1; i<nums.size();i++){
-            if(!nums[i]){
-                ma=mi=nums[i];
-            }else{
-                double x = ma,y=mi;
-                ma = max((double)nums[i],max(y*nums[i],x*nums[i]));
-                mi = min((double)nums[i],min(y*nums[i],x*nums[i]));
-            }
-            ans = max(ans,ma);
+        double l = 0, r = 0;
+        double ans = nums[0];
+        for(int i=0; i<nums.size();i++){
+            l = (l?l:1)*nums[i];
+            r = (r?r:1)*nums[nums.size()-i-1];
+            ans = max(ans,max(l,r));
         }
         return ans;
     }
