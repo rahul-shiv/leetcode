7afa49287e43c0e0e11a72eed044d8c2b0a06ef3
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-09-29 13:45:51 -0700

    Runtime: 0 ms (100.00%), Memory: 7.5 MB (11.97%) - Gitcode

---
+++ 96-unique-binary-search-trees/96. Unique Binary Search Trees.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i = 2;i<=n;i++){
+            for(int l=0,r;l<=i-1;l++){
+                r=i-l-1;
+                dp[i]+=dp[l]*dp[r];
+            }
+        }
+        return dp[n];
+    }
+};
