d5941cef1b162ab8ea426c7108431be156f1bc76
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-09 23:14:31 -0700

    Runtime: 10 ms (79.24%), Memory: 17.1 MB (26.27%) - Gitcode

--- 253-meeting-rooms-ii/253. Meeting Rooms II.cpp
+++ 253-meeting-rooms-ii/253. Meeting Rooms II.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
         int ans = 0, n = intervals.size();
-        int arr[(unsigned long)1e6+2]={0};
+        map<int,int> m;
         int l = INT_MAX, r = 0;
         for(int i = 0; i < n; i++){
-            l = min(l,intervals[i][0]);
-            r = max(r,intervals[i][1]);
-            arr[intervals[i][0]]+=1;
-            arr[intervals[i][1]]-=1;
+            m[intervals[i][0]]+=1;
+            m[intervals[i][1]]-=1;
         }
         int curr = 0;
-        for(int i = l; i < r+1; i++){
-            curr+=arr[i];
+        for(auto x:m){
+            curr+=x.second;
             ans = max(curr,ans);
         }
         return ans;
