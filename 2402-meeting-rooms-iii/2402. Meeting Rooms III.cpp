55d508e80fe48aae212eeafc2a15e4ff8ffaeaea
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-09 23:02:50 -0700

    Runtime: 406 ms (12.36%), Memory: 134.2 MB (9.38%) - Gitcode

--- 2402-meeting-rooms-iii/2402. Meeting Rooms III.cpp
+++ 2402-meeting-rooms-iii/2402. Meeting Rooms III.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-    Solution(){
-         ios_base::sync_with_stdio(false);
-         cin.tie(NULL);
-         cout.tie(NULL);
-    }
     int mostBooked(int n, vector<vector<int>>& meetings) {
         sort(meetings.begin(),meetings.end());
         priority_queue<pair<uint64_t,int>,vector<pair<uint64_t,int>>,greater<pair<uint64_t,int>>> pq;
         pair<uint64_t,int> p;
-        priority_queue<int> s;
+        set<int> s;
         uint64_t d;
-        int ans = 0, room;
+        int ans = 0;
         vector<int> v(n);
         for(int i = 0; i <n;i++){
-            s.push(-i);
+            s.insert(i);
         }
         for(auto m:meetings){
             d = 0;
             while(!pq.empty() and m[0]>=pq.top().first){
                 p = pq.top();
                 pq.pop();
-                s.push(-p.second);
+                s.insert(p.second);
             }
             if(pq.size()==n){
                 p = pq.top();
                 pq.pop();
                 d=max((uint64_t)0,p.first-m[0]);
-                s.push(-p.second);
+                s.insert(p.second);
             }
-            room = -s.top();
-            pq.push({m[1]+d,room});
-            v[room]++;
-            if(v[room]>v[ans]){
-                ans=room;
-            }else if(v[room]==v[ans]){
-                ans = min(room,ans);
+            auto it = s.begin();
+            pq.push({m[1]+d,*it});
+            v[*it]++;
+            if(v[*it]>v[ans]){
+                ans=*it;
+            }else if(v[*it]==v[ans]){
+                ans = min(*it,ans);
             }
-            s.pop();
+            s.erase(it);
         }
         return ans;
     }
