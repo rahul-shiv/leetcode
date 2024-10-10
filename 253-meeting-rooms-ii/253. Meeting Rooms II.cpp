9ea9df6e8adc05e5645b03d2f8049adb7ec1499e
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-09 23:12:24 -0700

    Runtime: 20 ms (16.64%), Memory: 19.4 MB (9.45%) - Gitcode

--- 253-meeting-rooms-ii/253. Meeting Rooms II.cpp
+++ 253-meeting-rooms-ii/253. Meeting Rooms II.cpp
@@ -3,12 +3,15 @@ public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
         int ans = 0, n = intervals.size();
         int arr[(unsigned long)1e6+2]={0};
+        int l = INT_MAX, r = 0;
         for(int i = 0; i < n; i++){
+            l = min(l,intervals[i][0]);
+            r = max(r,intervals[i][1]);
             arr[intervals[i][0]]+=1;
             arr[intervals[i][1]]-=1;
         }
         int curr = 0;
-        for(int i = 0; i < 1e6+2; i++){
+        for(int i = l; i < r+1; i++){
             curr+=arr[i];
             ans = max(curr,ans);
         }
