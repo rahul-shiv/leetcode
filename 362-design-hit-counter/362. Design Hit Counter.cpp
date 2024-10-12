1d17c108b0bad4a9d4c015eccf0b9c5a9230feca
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-12 14:55:04 -0700

    Runtime: 5 ms (11.28%), Memory: 9.6 MB (19.25%) - Gitcode

---
+++ 362-design-hit-counter/362. Design Hit Counter.cpp
@@ -0,0 +1,46 @@
+class HitCounter {
+        map<int,int> m;
+public:
+    HitCounter() {
+        m[0]=0;
+    }
+    
+    void clearOld(int timestamp){
+        auto it = m.upper_bound(timestamp-300);
+        if(it!=m.begin()){
+            it--;
+            m.erase(m.begin(),it);
+        }
+    }
+
+    void hit(int timestamp) {
+        clearOld(timestamp);
+        if(m.empty()){
+            m[timestamp]++;
+        }else{
+            m[timestamp]=m.rbegin()->second+1;
+        }
+    }
+    
+    int getHits(int timestamp) {
+        clearOld(timestamp);
+        auto it = m.begin();
+        auto it2 = m.upper_bound(timestamp);
+        if(it2==m.begin()){return 0;}
+        it2--;
+        if(it==it2 and it->first>timestamp-300){
+            cout<<it2->second<<endl;
+            return it2->second;
+        }else{
+            cout<<it2->second-it->second<<endl;
+            return it2->second-it->second;
+        }
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
