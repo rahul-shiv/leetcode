ce69d4e704e84cb445c7a22c18b4d8ae648409a3
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-13 16:04:00 -0700

    Runtime: 91 ms (87.36%), Memory: 71.4 MB (50.72%) - Gitcode

--- 1762-buildings-with-an-ocean-view/1762. Buildings With an Ocean View.cpp
+++ 1762-buildings-with-an-ocean-view/1762. Buildings With an Ocean View.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     vector<int> findBuildings(vector<int>& heights) {
         vector<int> ans;
-        for(int i = 0; i < heights.size(); i++){
-            while(!ans.empty() and heights[ans.back()]<=heights[i])ans.pop_back();
+        for(int i = 0; i < heights.size();i++){
+            while(!ans.empty() and heights[i]>=heights[ans.back()]){
+                ans.pop_back();
+            }
             ans.push_back(i);
         }
         return ans;
