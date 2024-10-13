1482ff877b4e3c482365cef8fb757f1c5d00de05
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-13 16:27:55 -0700

    Runtime: 342 ms (72.98%), Memory: 179.1 MB (35.45%) - Gitcode

---
+++ 146-lru-cache/146. LRU Cache.cpp
@@ -0,0 +1,42 @@
+class LRUCache {
+    list<int> l;
+    unordered_map<int,pair<int,list<int>::iterator>> m;
+    int capacity;
+public:
+    LRUCache(int capacity): capacity(capacity) {
+    }
+    
+    int get(int key) {
+        auto it = m.find(key);
+        if(it==m.end()){
+            return -1;
+        }
+        l.erase(it->second.second);
+        l.push_back(key);
+        it->second.second = prev(l.end());
+        return it->second.first;
+    }
+    
+    void put(int key, int value) {
+        auto it = m.find(key);
+        if(it==m.end()){
+            if(l.size()==capacity){
+                m.erase(l.front());
+                l.pop_front();
+            }
+            l.push_back(key);
+            m[key] = make_pair(value,prev(l.end()));
+        }else{
+            l.erase(it->second.second);
+            l.push_back(key);
+            it->second = make_pair(value,prev(l.end()));
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
