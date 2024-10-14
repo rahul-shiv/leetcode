16866384c98a9e8726a6a263d7e94353c70699ab
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-13 21:54:33 -0700

    Runtime: 23 ms (77.52%), Memory: 16.4 MB (73.32%) - Gitcode

--- 322-coin-change/322. Coin Change.cpp
+++ 322-coin-change/322. Coin Change.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if(!amount)return 0;
+        vector<int> ans(amount+1,2e4);
+        ans[0]=0;
         sort(coins.begin(),coins.end());
-        vector<int> dp(amount+1);
-        queue<int> q;
-        q.push(0);
-        int u,v;
-        while(!q.empty()){
-            u = q.front();
-            q.pop();
-            for(auto coin:coins){
-                v = u+coin;
-                if(v==amount) return dp[u]+1;
-                if(v<amount and !dp[v]){
-                    dp[v]=dp[u]+1;
-                    q.push(v);
-                }
+        for(auto coin:coins){
+            for(int i = coin; i<=amount;i++){
+                ans[i] = min(ans[i],ans[i-coin]+1);
             }
         }
-        return -1;
+        return ans[amount]==2e4?-1:ans[amount];
     }
 };
