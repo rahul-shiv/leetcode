7bd5323323e8f022dbe279490dd21f569c90428f
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-13 21:41:35 -0700

    Runtime: 78 ms (31.87%), Memory: 27.5 MB (51.72%) - Gitcode

--- 15-3sum/15. 3Sum.cpp
+++ 15-3sum/15. 3Sum.cpp
@@ -6,8 +6,8 @@ public:
         sort(nums.begin(),nums.end());
         int n = nums.size(),l,r;
         for(int i = 0; i < n-2; i++){
-            if(i){
-                while(i<n and nums[i]==nums[i-1])i++;
+            if(i and nums[i]==nums[i-1]){
+                continue;
             }
             l=i+1;
             r = n-1;
