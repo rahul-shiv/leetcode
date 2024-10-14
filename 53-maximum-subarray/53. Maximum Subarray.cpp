76d30cc079126d59b896526017895258691c516a
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-13 17:10:46 -0700

    Runtime: 73 ms (75.23%), Memory: 70.7 MB (20.84%) - Gitcode

--- 53-maximum-subarray/53. Maximum Subarray.cpp
+++ 53-maximum-subarray/53. Maximum Subarray.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int m = nums[0], ans = nums[0];
-        for(int i = 1; i < nums.size(); i++){
-            m = max(nums[i],m+nums[i]);
-            ans = max(ans, m);
+        int ans = -1e4-1, s = -1e4-1;
+        for(auto num:nums){
+            s = max(num,s+num);
+            ans = max(ans,s);
         }
         return ans;
     }
