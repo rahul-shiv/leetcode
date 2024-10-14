7dc719b98b057749837b8db281ff9a20f6cb2ac7
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-14 08:12:06 -0700

    Runtime: 19 ms (50.07%), Memory: 19.4 MB (26.15%) - Gitcode

--- 207-course-schedule/207. Course Schedule.cpp
+++ 207-course-schedule/207. Course Schedule.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> g(numCourses);
         vector<int> indegree(numCourses);
+        vector<vector<int>> g(numCourses);
         queue<int> q;
         for(auto p:prerequisites){
-            g[p[1]].push_back(p[0]);
             indegree[p[0]]++;
+            g[p[1]].push_back(p[0]);
         }
-        for(int i = 0; i<numCourses;i++){
-            if(!indegree[i])q.push(i);
+        for(int i = 0; i < numCourses; i++){
+            if(!indegree[i]) q.push(i);
         }
         int u;
         while(!q.empty()){
             u = q.front();
-            q.pop();
             numCourses--;
+            q.pop();
             for(auto v:g[u]){
                 indegree[v]--;
-                if(!indegree[v])q.push(v);
+                if(!indegree[v]){
+                    q.push(v);
+                }
             }
         }
-        return numCourses!=0?false:true;
+        return !numCourses;
     }
 };
