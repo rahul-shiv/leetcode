c749fb7e7d40b7023bd8d4f5c154e4df561111e6
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-13 21:40:37 -0700

    Runtime: 92 ms (28.08%), Memory: 27.4 MB (89.73%) - Gitcode

--- 15-3sum/15. 3Sum.cpp
+++ 15-3sum/15. 3Sum.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
+    #define SUM3 (nums[l]+nums[r]+nums[i])
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size(), l, r, s;
         vector<vector<int>> ans;
-        for(int i = 0 ; i < n - 2; i++){
-            if(i and nums[i]==nums[i-1])continue;
-            l = i+1;
+        sort(nums.begin(),nums.end());
+        int n = nums.size(),l,r;
+        for(int i = 0; i < n-2; i++){
+            if(i){
+                while(i<n and nums[i]==nums[i-1])i++;
+            }
+            l=i+1;
             r = n-1;
             while(l<r){
-                s = nums[i]+nums[l]+nums[r];
-                if(s==0){
+                if(SUM3==0){
                     ans.push_back({nums[i],nums[l],nums[r]});
+                    
                 }
-                if(s<=0){
+                if(SUM3<=0){
                     l++;
-                    while(l<r and nums[l]==nums[l-1])l++;
+                    while(l<r and nums[l]==nums[l-1]){
+                        l++;
+                    }
                 }else{
                     r--;
-                    while(l<r and nums[r]==nums[r+1])r--;
+                    while(l<r and nums[r]==nums[r+1]){
+                        r--;
+                    }
                 }
             }
         }
