947d9ee16e460038a2d74752e8bac5354c5b6765
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-13 20:53:53 -0700

    Runtime: 369 ms (9.59%), Memory: 60.9 MB (92.72%) - Gitcode

--- 973-k-closest-points-to-origin/973. K Closest Points to Origin.cpp
+++ 973-k-closest-points-to-origin/973. K Closest Points to Origin.cpp
@@ -1,34 +1,35 @@
 class Solution {
-    int dist(vector<int> &x){
-        return x[0]*x[0]+x[1]*x[1];
-    }
-    int partition(vector<vector<int>>& nums, int l, int r){
-        int t = rand()%(r-l+1)+l;
-        swap(nums[r],nums[t]);
-        int p = l;
-        int x = dist(nums[r]);
+    #define DIST(point) (point[0]*point[0]+point[1]*point[1])
+    int partition(vector<vector<int>>& points, int l, int r){
         for(int i = l; i<r;i++){
-            if(dist(nums[i])<=x){
-                swap(nums[p++],nums[i]);
+            if(DIST(points[i])<DIST(points[r])){
+                swap(points[i],points[l++]);
             }
         }
-        swap(nums[r],nums[p]);
-        return p;
+        swap(points[r],points[l]);
+        return l;
     }
-    void qsort(vector<vector<int>>& points, int k, int l, int r){
-        if(l>=r or r<l)return;
-        int p = r;
-        while(p!=k){
-            p = partition(points,l,r);
-            if(p==k)return;
-            else if(p<k) l=p+1;
-            else r=p-1;
+    void kClosest(vector<vector<int>>& points, int l, int r,int k){
+        int piv;
+        while(l<r){
+            piv = rand()%(r-l)+l;
+            swap(points[r],points[piv]);
+            piv = partition(points,l,r);
+            if(piv>k){
+                r = piv-1;
+            }else if (piv<k){
+                l = piv+1;
+            }else{
+                return;
+            }
         }
     }
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        qsort(points,k-1,0,points.size()-1);
-        vector<vector<int>> ans(points.begin(),points.begin()+k);
+        srand(time(nullptr));
+        vector<vector<int>> ans;
+        kClosest(points,0,points.size()-1,k-1);
+        ans = vector<vector<int>>(points.begin(),points.begin()+k);
         return ans;
     }
 };
