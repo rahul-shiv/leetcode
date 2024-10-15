7d81abdbf8fd52bc143e89f48f339af881bd0079
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-15 16:33:10 -0700

    Runtime: 83 ms (48.54%), Memory: 47.7 MB (39.17%) - Gitcode

--- 560-subarray-sum-equals-k/560. Subarray Sum Equals K.cpp
+++ 560-subarray-sum-equals-k/560. Subarray Sum Equals K.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> m;
-        m[0]=1;
+        map<int,int> m;
         int ans = 0;
-        for(int i = 0;i<nums.size();i++){
-            if(i){
-                nums[i]+=nums[i-1];
-            }
-            if(m[nums[i]-k]){
-                ans+=m[nums[i]-k];
-            }
+        m[0]=1;
+        for(int i =0; i<nums.size();i++){
+            if(i)nums[i]+=nums[i-1];
+            auto it = m.find(nums[i]-k);
+            if(it!=m.end())ans+=it->second;
             m[nums[i]]++;
         }
         return ans;
