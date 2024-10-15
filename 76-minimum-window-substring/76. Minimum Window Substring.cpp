845ca9ceacbf602e53d21260b28a9ade01a9e2c8
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-15 15:46:18 -0700

    Runtime: 8 ms (82.58%), Memory: 10.2 MB (79.18%) - Gitcode

--- 76-minimum-window-substring/76. Minimum Window Substring.cpp
+++ 76-minimum-window-substring/76. Minimum Window Substring.cpp
@@ -1,29 +1,26 @@
 class Solution {
-    bool cmp(vector<int>&sv, vector<int>&tv){
-        for(int i = 0; i < 128;i++){
-            if(sv[i]<tv[i])return false;
-        }
-        return true;
-    }
 public:
     string minWindow(string s, string t) {
         vector<int> tv(128),sv(128);
-        int n = s.length();
+        int n = s.length(), count=0;
         for(auto c:t){
+            if(!tv[c-'A'])count++;
             tv[c-'A']+=1;
         }
         int l=0,r=0,minl=INT_MAX,start=-1;
         while(r<n){
-            while(r<n and !cmp(sv,tv)){
+            while(r<n and count){
                 sv[s[r]-'A']++;
+                if(tv[s[r]-'A'] and tv[s[r]-'A']==sv[s[r]-'A']) count--;
                 r++;
             }
-            while(l<r and cmp(sv,tv)){
+            while(l<r and count==0){
                 if(r-l<minl){
                     minl=r-l;
                     start=l;
                 }
                 sv[s[l]-'A']--;
+                if(tv[s[l]-'A'] and tv[s[l]-'A']>sv[s[l]-'A']) count++;
                 l++;
             }
         }
