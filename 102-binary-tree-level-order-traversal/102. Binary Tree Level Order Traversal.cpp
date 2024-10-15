321dd9fa9e8fe6a3cec863c63f63c2821085528e
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-15 16:43:45 -0700

    Runtime: 0 ms (100.00%), Memory: 14.9 MB (94.42%) - Gitcode

--- 102-binary-tree-level-order-traversal/102. Binary Tree Level Order Traversal.cpp
+++ 102-binary-tree-level-order-traversal/102. Binary Tree Level Order Traversal.cpp
@@ -14,29 +14,19 @@ public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         if(!root)return {};
         vector<vector<int>> ans;
-        vector<vector<TreeNode*>> tr;
-        ans.push_back({root->val});
-        tr.push_back({root});
-        int i = 0;
-        auto it = tr.begin();
-        while(i<tr.size()){
-            for(auto node:tr[i]){
-                if(node->left||node->right){
-                    if(tr.size()-1==i){
-                        tr.push_back({});
-                        ans.push_back({});
-                    }
-                    if(node->left){
-                        tr.back().push_back(node->left);
-                        ans.back().push_back({node->left->val});
-                    }
-                    if(node->right){
-                        tr.back().push_back(node->right);
-                        ans.back().push_back({node->right->val});
-                    }
-                }
+        queue<pair<TreeNode* ,int>> q;
+        q.push({root,0});
+        TreeNode * node;
+        int d;
+        while(!q.empty()){
+            tie(node,d) = q.front();
+            q.pop();
+            if(d==ans.size()){
+                ans.push_back({});
             }
-            i++;
+            ans[d].push_back(node->val);
+            if(node->left)q.push({node->left,d+1});
+            if(node->right)q.push({node->right,d+1});
         }
         return ans;
     }
