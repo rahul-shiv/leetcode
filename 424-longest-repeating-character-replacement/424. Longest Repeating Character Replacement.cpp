66a6a9b977a80953f26e53b039a3dea74ccee5ed
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-16 16:24:51 -0700

    Runtime: 15 ms (23.67%), Memory: 9.7 MB (19.76%) - Gitcode

--- 424-longest-repeating-character-replacement/424. Longest Repeating Character Replacement.cpp
+++ 424-longest-repeating-character-replacement/424. Longest Repeating Character Replacement.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int l = 0, r = 0, n = s.length(),ans = 0,m=0;
-        vector<int> c(26);
+        int l=0,r=1,n=s.size(), ans = 1,x=0;
+        char currm = s[0];
+        unordered_map<char,int> m;
+        m[s[0]]+=1;
         while(r<n){
-            ans = max(ans,r-l);
-            c[s[r]-'A']+=1;
-            m = max(m,c[s[r]-'A']);
-            r+=1;
-            if(r-l-m>k){
-                c[s[l]-'A']-=1;
-                l+=1;
+            m[s[r]]++;
+            if(s[r]!=currm){
+                x++;
+                if(m[s[r]]>=m[currm]){
+                    currm = s[r];
+                    x = (r-l+1)-m[s[r]];
+                }
             }
+            while(x>k){
+                m[s[l]]--;
+                if(s[l]==currm){
+                    int t = 0;
+                    for(auto temp:m){
+                        if(temp.second>t){
+                            t=temp.second;
+                            currm=temp.first;
+                        }
+                    }
+                    x = (r-l)-m[currm];
+                }else{
+                    x--;
+                }
+                l++;
+            }
+            ans = max(r-l+1,ans);
+            r++;
         }
-        ans = max(ans,r-l);
         return ans;
     }
 };
