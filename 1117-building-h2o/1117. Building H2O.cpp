fc7010d4addfd2212c488fab73c8d8fbcf220846
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-17 15:15:18 -0700

    Runtime: 13 ms (15.86%), Memory: 11.2 MB (24.97%) - Gitcode

--- 1117-building-h2o/1117. Building H2O.cpp
+++ 1117-building-h2o/1117. Building H2O.cpp
@@ -1,38 +1,28 @@
 class H2O {
-    int hc;
-    int oc;
+    int h;
     mutex m;
     condition_variable cv;
 public:
     H2O() {
-        oc=hc=0;
+        h = 0;
     }
 
     void hydrogen(function<void()> releaseHydrogen) {
         unique_lock<mutex> guard(m);
-        while(hc>1){
-            cv.wait(guard);
-        }
+        cv.wait(guard,[this](){return h<2;});
+        h++;
         // releaseHydrogen() outputs "H". Do not change or remove this line.
         releaseHydrogen();
-        hc+=1;
-        if(oc==1 and hc==2){
-            oc = hc = 0;
-        }
         cv.notify_all();
     }
 
     void oxygen(function<void()> releaseOxygen) {
         unique_lock<mutex> guard(m);
-        while(oc>0){
-            cv.wait(guard);
-        }
+        cv.wait(guard,[this](){return h==2;});
+        h=0;
+        
         // releaseOxygen() outputs "O". Do not change or remove this line.
-        oc+=1;
         releaseOxygen();
-        if(oc==1 and hc==2){
-            oc = hc = 0;
-        }
         cv.notify_all();
     }
 };
