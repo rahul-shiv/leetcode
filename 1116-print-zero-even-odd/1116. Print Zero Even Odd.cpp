adcb74553e9867be84fac665d929209195d59431
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-17 15:10:32 -0700

    Runtime: 0 ms (100.00%), Memory: 9.6 MB (23.38%) - Gitcode

--- 1116-print-zero-even-odd/1116. Print Zero Even Odd.cpp
+++ 1116-print-zero-even-odd/1116. Print Zero Even Odd.cpp
@@ -1,7 +1,7 @@
 class ZeroEvenOdd {
 private:
     int n;
-    int i,j;
+    int i;
     bool pzero;
     mutex m;
     condition_variable cv;
@@ -9,21 +9,18 @@ private:
 public:
     ZeroEvenOdd(int n) {
         this->n = n;
-        pzero = true;
         i = 1;
-        j = 1;
+        pzero=true;
     }
 
     // printNumber(x) outputs "x", where x is an integer.
     void zero(function<void(int)> printNumber) {
         while(true){
             unique_lock<mutex> guard(m);
-            cv.wait(guard,[this](){
-                return i>n or pzero;
-            });
-            if(i>n)break;
+            cv.wait(guard,[this](){return i>n or pzero;});
+            if(i>n){break;}
             printNumber(0);
-            this->pzero=false;
+            pzero=false;
             cv.notify_all();
         }
     }
@@ -31,12 +28,10 @@ public:
     void even(function<void(int)> printNumber) {
         while(true){
             unique_lock<mutex> guard(m);
-            cv.wait(guard,[this](){
-                return i>n or (!pzero and i%2==0);
-            });
-            if(i>n)break;
+            cv.wait(guard,[this](){return i>n or (i%2==0 and !pzero);});
+            if(i>n){break;}
             printNumber(i++);
-            this->pzero=true;
+            pzero=true;
             cv.notify_all();
         }
     }
