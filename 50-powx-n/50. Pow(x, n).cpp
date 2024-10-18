5c1620f45aa0f5d1d53ce53cebe4bd99470268b8
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-17 17:26:06 -0700

    Runtime: 0 ms (100.00%), Memory: 8.3 MB (72.00%) - Gitcode

--- 50-powx-n/50. Pow(x, n).cpp
+++ 50-powx-n/50. Pow(x, n).cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==1||x==1)return x;
         if(n==0)return 1;
         if(x==0)return 0;
         if(n<0){x=1/x;}
         double t = 1;
-        while(abs(n)>1){
+        while(n){
             if(n%2){
                 t *= x;
             }
             x*=x;
             n = abs(n/2);
         }
-        return t*x;
+        return t;
     }
 };
