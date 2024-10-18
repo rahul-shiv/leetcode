2dd0ab775313e3f1ee062c44f600355dea15652e
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-17 17:21:12 -0700

    Runtime: 0 ms (100.00%), Memory: 8.3 MB (72.00%) - Gitcode

--- 50-powx-n/50. Pow(x, n).cpp
+++ 50-powx-n/50. Pow(x, n).cpp
@@ -5,10 +5,14 @@ public:
         if(n==0)return 1;
         if(x==0)return 0;
         if(n<0){x=1/x;}
-        if(n%2){
-            return x*myPow(x*x,abs(n/2));
-        }else{
-            return myPow(x*x,abs(n/2));
+        double t = 1;
+        while(abs(n)>1){
+            if(n%2){
+                t *= x;
+            }
+            x*=x;
+            n = abs(n/2);
         }
+        return t*x;
     }
 };
