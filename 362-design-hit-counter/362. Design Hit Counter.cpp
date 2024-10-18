27a30e55d2eba9ad0b37692e64cc49805d9ce0d2
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-18 16:54:40 -0700

    Runtime: 5 ms (11.53%), Memory: 9.6 MB (20.29%) - Gitcode

--- 362-design-hit-counter/362. Design Hit Counter.cpp
+++ 362-design-hit-counter/362. Design Hit Counter.cpp
@@ -1,12 +1,12 @@
 class HitCounter {
-        vector<pair<int,int>> m;
+        map<int,int> m;
 public:
     HitCounter() {
-        m.emplace_back(make_pair(0,0));
+        m[0]=0;
     }
     
     void clearOld(int timestamp){
-        auto it = upper_bound(m.begin(),m.end(),make_pair(timestamp-300,INT_MAX));
+        auto it = m.upper_bound(timestamp-300);
         if(it!=m.begin()){
             it--;
             m.erase(m.begin(),it);
@@ -16,16 +16,16 @@ public:
     void hit(int timestamp) {
         clearOld(timestamp);
         if(m.empty()){
-            m.emplace_back(make_pair(timestamp,0));
+            m[timestamp]++;
         }else{
-            m.emplace_back(make_pair(timestamp,m.back().second+1));
+            m[timestamp]=m.rbegin()->second+1;
         }
     }
     
     int getHits(int timestamp) {
         clearOld(timestamp);
         auto it = m.begin();
-        auto it2 = m.end();
+        auto it2 = m.upper_bound(timestamp);
         if(it2==m.begin()){return 0;}
         it2--;
         if(it==it2 and it->first>timestamp-300){
