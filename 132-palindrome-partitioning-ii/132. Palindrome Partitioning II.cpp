c3d8b88927fce80f68109e21e1fa69742c14da7f
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-17 21:49:43 -0700

    Runtime: 134 ms (64.26%), Memory: 8.1 MB (75.83%) - Gitcode

--- 132-palindrome-partitioning-ii/132. Palindrome Partitioning II.cpp
+++ 132-palindrome-partitioning-ii/132. Palindrome Partitioning II.cpp
@@ -17,7 +17,7 @@ public:
         for(int i = 0;i<n+1;i++)ans[i]=i-1;
         for(int i = 0;i<n;i++){
             for(int j = i;j<n;j++){
-                if(isPalindrome(s,i,j)){
+                if(ans[j+1]>ans[i]+1 and isPalindrome(s,i,j)){
                     ans[j+1]=min(ans[j+1],ans[i]+1);
                 }
             }
