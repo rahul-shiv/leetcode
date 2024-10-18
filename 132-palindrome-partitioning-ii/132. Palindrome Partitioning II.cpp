8260863eabd989790cc4182bca7a40c74a2fd447
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-17 21:48:33 -0700

    Runtime: 1292 ms (54.16%), Memory: 8 MB (75.83%) - Gitcode

---
+++ 132-palindrome-partitioning-ii/132. Palindrome Partitioning II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    bool isPalindrome(string &s, int i, int j){
+        int x = i,y = j;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+public:
+    int minCut(string s) {
+        int n = s.length();
+        vector<int> ans(n+1);
+        for(int i = 0;i<n+1;i++)ans[i]=i-1;
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
+                if(isPalindrome(s,i,j)){
+                    ans[j+1]=min(ans[j+1],ans[i]+1);
+                }
+            }
+        }
+        return ans[n];
+    }
+};
