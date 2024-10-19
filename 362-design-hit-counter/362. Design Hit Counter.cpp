87eed9843128c9f1c6c0108023fec6127affba5d
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-19 11:02:29 -0700

    Runtime: 0 ms (100.00%), Memory: 9.6 MB (20.29%) - Gitcode

--- 362-design-hit-counter/362. Design Hit Counter.cpp
+++ 362-design-hit-counter/362. Design Hit Counter.cpp
@@ -1,40 +1,28 @@
 class HitCounter {
-        vector<pair<int,int>> m;
+        deque<pair<int,int>> m;
+        int total;
 public:
     HitCounter() {
-        m.emplace_back(make_pair(0,0));
-    }
-    
-    void clearOld(int timestamp){
-        auto it = upper_bound(m.begin(),m.end(),make_pair(timestamp-300,INT_MAX));
-        if(it!=m.begin()){
-            it--;
-            m.erase(m.begin(),it);
-        }
+        total = 0;
     }
 
     void hit(int timestamp) {
-        clearOld(timestamp);
-        if(m.empty()){
-            m.emplace_back(make_pair(timestamp,0));
+        if(!m.empty() and m.back().first==timestamp){
+            m.back().second+=1;
         }else{
-            m.emplace_back(make_pair(timestamp,m.back().second+1));
+            m.emplace_back(make_pair(timestamp,1));
         }
+        total+=1;
     }
     
     int getHits(int timestamp) {
-        clearOld(timestamp);
-        auto it = m.begin();
-        auto it2 = m.end();
-        if(it2==m.begin()){return 0;}
-        it2--;
-        if(it==it2 and it->first>timestamp-300){
-            cout<<it2->second<<endl;
-            return it2->second;
-        }else{
-            cout<<it2->second-it->second<<endl;
-            return it2->second-it->second;
+        for(auto &x:m){
+            if(x.first>timestamp-300)break;
+            total-=x.second;
+            m.erase(m.begin());
         }
+
+        return total;
     }
 };
 
