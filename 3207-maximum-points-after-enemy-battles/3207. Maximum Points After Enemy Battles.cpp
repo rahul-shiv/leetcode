1a314895022f38de1d0f0dc16c32e96924936ef2
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-25 09:57:11 -0700

    Runtime: 30 ms (95.44%), Memory: 76.7 MB (8.35%) - Gitcode

--- 3207-maximum-points-after-enemy-battles/3207. Maximum Points After Enemy Battles.cpp
+++ 3207-maximum-points-after-enemy-battles/3207. Maximum Points After Enemy Battles.cpp
@@ -5,7 +5,7 @@ public:
         sort(enemyEnergies.begin(),enemyEnergies.end());
         int l = 0, r = enemyEnergies.size()-1;
         while(l<=r){
-            while(currentEnergy>=enemyEnergies[l]){
+            if(currentEnergy>=enemyEnergies[l]){
                 ans+=currentEnergy/enemyEnergies[l];
                 currentEnergy-=currentEnergy/enemyEnergies[l]*enemyEnergies[l];
             }
