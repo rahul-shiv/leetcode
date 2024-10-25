81630152de4d8ea53fd1e35fab8cd7a6615862cf
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-24 21:20:10 -0700

    Runtime: 18 ms (63.29%), Memory: 21.3 MB (62.88%) - Gitcode

---
+++ 87-scramble-string/87. Scramble String.cpp
@@ -0,0 +1,39 @@
+class Solution {
+   unordered_map<string, bool> memo;
+    
+    bool solve(const string &s1, const string &s2) {
+        if (s1 == s2) return true;
+        if (s1.length() != s2.length()) return false;
+
+        string key = s1 + "," + s2;
+        if (memo.find(key) != memo.end()) return memo[key];
+
+        int n = s1.length();
+        vector<int> c1(26), c2(26);
+
+        // Count character frequencies to check if s1 and s2 have the same characters
+        for (int i = 0; i < n; i++) {
+            c1[s1[i] - 'a']++;
+            c2[s2[i] - 'a']++;
+        }
+        if (c1 != c2) return memo[key] = false;
+
+        // Check all possible splits
+        for (int i = 1; i < n; i++) {
+            // Check without swapping
+            if (solve(s1.substr(0, i), s2.substr(0, i)) && solve(s1.substr(i), s2.substr(i))) {
+                return memo[key] = true;
+            }
+            // Check with swapping
+            if (solve(s1.substr(0, i), s2.substr(n - i)) && solve(s1.substr(i), s2.substr(0, n - i))) {
+                return memo[key] = true;
+            }
+        }
+        return memo[key] = false;
+    }
+
+public:
+    bool isScramble(string s1, string s2) {
+        return solve(s1,s2);
+    }
+};
