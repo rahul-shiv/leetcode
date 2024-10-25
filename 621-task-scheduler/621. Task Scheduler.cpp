02605371b93a7f2fcf82241fbd867b02d379b10d
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-10-24 20:51:56 -0700

    Runtime: 23 ms (96.37%), Memory: 38.1 MB (95.25%) - Gitcode

--- 621-task-scheduler/621. Task Scheduler.cpp
+++ 621-task-scheduler/621. Task Scheduler.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        map<char,int> m;
-        for(auto x:tasks){
-            m[x]++;
+        unordered_map<char,int> m;
+        int mc = 0, maxc=0;
+        for(auto &task:tasks){
+            m[task]++;
+            if(m[task]>mc){
+                mc=m[task];
+                maxc=1;
+            }else if(m[task]==mc){
+                maxc++;
+            }
         }
-        set<pair<int,char>,greater<pair<int,char>>> s;
-        int ans = 0;
-        map<int,int> q;
-        for(auto k:m){
-            q[k.second]++;
-        }
-        return max((q.rbegin()->first-1)*(n+1)+q.rbegin()->second,(int)tasks.size());
+        return max((mc-1)*(n+1)+maxc,(int)tasks.size());
     }
 };
