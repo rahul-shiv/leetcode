362a461ef1b26427ae268f039be58f942841ae63
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-11-03 23:38:22 -0800

    Runtime: 75 ms (91.98%), Memory: 146.3 MB (31.01%) - Gitcode

--- 523-continuous-subarray-sum/523. Continuous Subarray Sum.cpp
+++ 523-continuous-subarray-sum/523. Continuous Subarray Sum.cpp
@@ -2,11 +2,17 @@ class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         unordered_map<int,int> x;
-        x[0]++;
-        for(int i = 1;i<nums.size();i++){
-            nums[i]+=nums[i-1];
-            if(x[nums[i]%k])return true;
-            x[nums[i-1]%k]++;
+        x[0]=-1;
+        int curr = 0;
+        for(int i = 0;i<nums.size();i++){
+            curr+=nums[i];
+            curr%=k;
+            auto it = x.find(curr);
+            if(it!=x.end()){
+                if(i-it->second>1)return true;
+            }else{
+                x[curr]=i;
+            }
         }
 
         return false;
