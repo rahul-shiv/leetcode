0907f091d1a9600c9498a3508070a7fa3d2b3ca8
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-11-04 20:07:03 -0800

    Runtime: 0 ms (100.00%), Memory: 9.1 MB (64.97%) - Gitcode

--- 32-longest-valid-parentheses/32. Longest Valid Parentheses.cpp
+++ 32-longest-valid-parentheses/32. Longest Valid Parentheses.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
+        int ans = 0, n = s.length();
         stack<int> st;
-        int ans = 0;
         st.push(-1);
-        for(int i = 0; i < s.length();i++){
-            if(s[i]=='('){
-                st.push(i);
-            }else if(st.size()>1){
-                st.pop();
-                ans = max(ans,i-st.top());
+        for(int i = 0;i<n;i++){
+            if(s[i]==')'){
+                if(st.size()==1){
+                    ans = max(ans,i-1-st.top());
+                    st.top()=i;
+                }else{
+                    st.pop();
+                    ans = max(ans,i-st.top());
+                }
             }else{
-                st.top()=i;
+                st.push(i);
             }
         }
+
         return ans;
     }
 };
