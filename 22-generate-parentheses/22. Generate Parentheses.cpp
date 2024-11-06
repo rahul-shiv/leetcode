9b00365ce6cede695386a19382bed6e7f11f95b0
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-11-05 19:03:51 -0800

    Runtime: 3 ms (70.73%), Memory: 10.1 MB (97.70%) - Gitcode

--- 22-generate-parentheses/22. Generate Parentheses.cpp
+++ 22-generate-parentheses/22. Generate Parentheses.cpp
@@ -4,16 +4,12 @@ public:
         unordered_map<int,unordered_set<string>> ans;
         ans[0].insert("");
         for(int i = 1;i<=n;i++){
-            for(auto &s:ans[i-1]){
-                ans[i].insert("("+s+")");
-                ans[i].insert("()"+s);
-                ans[i].insert(s+"()");
-            }
-            for(int j = 2, k=i-j; j<n and k>=j; j++,k--){
+            for(int j = 0, k=i-j-1; j<n; j++,k--){
                 for(auto &s1:ans[j]){
                     for(auto &s2:ans[k]){
-                        ans[i].insert(s1+s2);
-                        ans[i].insert(s2+s1);
+                        ans[i].insert("()"+s1+s2);
+                        ans[i].insert(s1+s2+"()");
+                        ans[i].insert("("+s1+")"+s2);
                     }
                 }
             }
