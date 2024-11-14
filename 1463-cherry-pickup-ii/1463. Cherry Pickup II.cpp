1951a81c07ad6f800c29d5ca5f9fe58c4ff38637
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-11-14 00:35:28 -0800

    Runtime: 159 ms (8.29%), Memory: 41.6 MB (5.58%) - Gitcode

--- 1463-cherry-pickup-ii/1463. Cherry Pickup II.cpp
+++ 1463-cherry-pickup-ii/1463. Cherry Pickup II.cpp
@@ -3,24 +3,23 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         int ans = 0;
-        vector<map<pair<int,int>,int>> g(m);
-        g[0][{0,n-1}]=grid[0][0]+grid[0][n-1];
+        vector<vector<vector<int>>> v(m,vector<vector<int>>(n,vector<int>(n)));
+        v[0][0][n-1]=grid[0][0]+grid[0][n-1];
+        vector<unordered_set<int>> frontier(m);
+        frontier[0].insert(n-1);
         int dirs[][2] = {{0,1},{1,0},{0,0},{1,1},{-1,-1},{-1,0},{0,-1},{-1,1},{1,-1}};
         for(int i = 1;i<m;i++){
-            for(auto &p:g[i-1]){
+            for(int x:frontier[i-1]){
+                int t1 = x/n;
+                int t2 = x%n;
                 for(auto &dir:dirs){
-                    int r1 = p.first.first+dir[0];
-                    int r2 = p.first.second+dir[1];
+                    int r1 = t1+dir[0];
+                    int r2 =t2+dir[1];
                     #define CHECK(x,y) r1>=0 and r2<n and r2>=0 and r2<n and r1<=r2
                     if(CHECK(r1,r2)){
-                        auto it = g[i].find({r1,r2});
-                        if(it==g[i].end()){
-                            g[i][{r1,r2}]=p.second+grid[i][r1]+(r1!=r2)*grid[i][r2];
-                            ans = max(ans,g[i][{r1,r2}]);
-                        }else{
-                            it->second = max(it->second, p.second+grid[i][r1]+(r1!=r2)*grid[i][r2]);
-                            ans = max(ans,it->second);
-                        }
+                        v[i][r1][r2] = max(v[i][r1][r2], v[i-1][t1][t2]+grid[i][r1]+(r1!=r2)*grid[i][r2]);
+                        ans = max(ans,v[i][r1][r2]);
+                        frontier[i].insert(r1*n+r2);
                     }
                 }
             }
