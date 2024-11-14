2a1464c5f757fb6a32bb2b851a13fdd21a7fe640
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-11-13 20:01:55 -0800

    Runtime: 164 ms (15.04%), Memory: 137.4 MB (66.70%) - Gitcode

--- 239-sliding-window-maximum/239. Sliding Window Maximum.cpp
+++ 239-sliding-window-maximum/239. Sliding Window Maximum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        list<pair<int,int>> q;
+        deque<int> q;
         vector<int> ans; 
         for(int i = 0;i<k-1;i++){
-            while(!q.empty() and q.back().first<nums[i])q.pop_back();
-            q.push_back({nums[i],i});
+            while(!q.empty() and nums[q.back()]<nums[i])q.pop_back();
+            q.push_back(i);
         }
         for(int i = k-1;i<nums.size();i++){
-            while(!q.empty() and q.front().second<i-k+1)q.pop_front();
-            while(!q.empty() and q.back().first<nums[i])q.pop_back();
-            q.push_back({nums[i],i});
-            ans.push_back(q.front().first);
+            while(!q.empty() and q.front()<i-k+1)q.pop_front();
+            while(!q.empty() and nums[q.back()]<nums[i])q.pop_back();
+            q.push_back(i);
+            ans.push_back(nums[q.front()]);
         }
         return ans;
         //redo with monotonic dequeue
