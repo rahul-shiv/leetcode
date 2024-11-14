15e92753aaecebc804aed3c8b59cff21ad9c38e2
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-11-14 01:15:09 -0800

    Runtime: 765 ms (18.98%), Memory: 551.8 MB (74.26%) - Gitcode

---
+++ 211-design-add-and-search-words-data-structure/211. Design Add and Search Words Data Structure.cpp
@@ -0,0 +1,55 @@
+class WordDictionary {
+    struct Trie{
+        bool terminal;
+        unordered_map<char,Trie*> m;
+    };
+    Trie t;
+    bool recursiveSearch(string word, Trie*temp){
+        for(int i = 0;i<word.length();i++){
+            char c = word[i];
+            if(c=='.'){
+                for(auto &x:temp->m){
+                    bool t = recursiveSearch(word.substr(i+1),x.second);
+                    if(t)return true;
+                }
+                return false;
+            }else{
+                auto it = temp->m.find(c);
+                if(it==temp->m.end())return false;
+                temp=it->second;
+            }
+        }
+        return temp->terminal;
+
+    }
+public:
+    WordDictionary() {
+        
+    }
+    
+    void addWord(string word) {
+        Trie * temp = &t;
+        for(auto c:word){
+            auto it = temp->m.find(c);
+            if(it==temp->m.end()){
+                temp->m[c]=new Trie();
+                temp = temp->m[c];
+            }else{
+                temp = it->second;
+            }
+        }
+        temp->terminal=true;
+    }
+    
+
+    bool search(string word) {
+        return recursiveSearch(word,&t);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
