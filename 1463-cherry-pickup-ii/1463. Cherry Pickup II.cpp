1f0d82a87407bb458f4b3d63cc82eedadc15bd0a
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-11-14 00:43:47 -0800

    Runtime: 143 ms (8.61%), Memory: 41.7 MB (5.58%) - Gitcode

--- 1463-cherry-pickup-ii/1463. Cherry Pickup II.cpp
+++ 1463-cherry-pickup-ii/1463. Cherry Pickup II.cpp
@@ -5,11 +5,12 @@ public:
         int ans = 0;
         vector<vector<vector<int>>> v(m,vector<vector<int>>(n,vector<int>(n)));
         v[0][0][n-1]=grid[0][0]+grid[0][n-1];
-        vector<unordered_set<int>> frontier(m);
-        frontier[0].insert(n-1);
+        unordered_set<int> frontier;
+        frontier.insert(n-1);
         int dirs[][2] = {{0,1},{1,0},{0,0},{1,1},{-1,-1},{-1,0},{0,-1},{-1,1},{1,-1}};
         for(int i = 1;i<m;i++){
-            for(int x:frontier[i-1]){
+            unordered_set<int> nfrontier;
+            for(int x:frontier){
                 int t1 = x/n;
                 int t2 = x%n;
                 for(auto &dir:dirs){
@@ -19,10 +20,11 @@ public:
                     if(CHECK(r1,r2)){
                         v[i][r1][r2] = max(v[i][r1][r2], v[i-1][t1][t2]+grid[i][r1]+(r1!=r2)*grid[i][r2]);
                         ans = max(ans,v[i][r1][r2]);
-                        frontier[i].insert(r1*n+r2);
+                        nfrontier.insert(r1*n+r2);
                     }
                 }
             }
+            frontier = move(nfrontier);
         }
 
         return ans;
