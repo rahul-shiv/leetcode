d6e47b662e928d44abd776ef79cf329508e22b1b
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-11-14 00:45:24 -0800

    Runtime: 147 ms (8.45%), Memory: 38.8 MB (6.77%) - Gitcode

--- 1463-cherry-pickup-ii/1463. Cherry Pickup II.cpp
+++ 1463-cherry-pickup-ii/1463. Cherry Pickup II.cpp
@@ -3,8 +3,8 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         int ans = 0;
-        vector<vector<vector<int>>> v(m,vector<vector<int>>(n,vector<int>(n)));
-        v[0][0][n-1]=grid[0][0]+grid[0][n-1];
+        vector<vector<int>> v(m,vector<int>(n*n));
+        v[0][n-1]=grid[0][0]+grid[0][n-1];
         unordered_set<int> frontier;
         frontier.insert(n-1);
         int dirs[][2] = {{0,1},{1,0},{0,0},{1,1},{-1,-1},{-1,0},{0,-1},{-1,1},{1,-1}};
@@ -18,8 +18,8 @@ public:
                     int r2 =t2+dir[1];
                     #define CHECK(x,y) r1>=0 and r2<n and r2>=0 and r2<n and r1<=r2
                     if(CHECK(r1,r2)){
-                        v[i][r1][r2] = max(v[i][r1][r2], v[i-1][t1][t2]+grid[i][r1]+(r1!=r2)*grid[i][r2]);
-                        ans = max(ans,v[i][r1][r2]);
+                        v[i][r1*n+r2] = max(v[i][r1*n+r2], v[i-1][t1*n+t2]+grid[i][r1]+(r1!=r2)*grid[i][r2]);
+                        ans = max(ans,v[i][r1*n+r2]);
                         nfrontier.insert(r1*n+r2);
                     }
                 }
