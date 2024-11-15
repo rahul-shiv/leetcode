f4be87937b33ea2dcea8974c1f1882d4d02d3bdc
rahul-shiv <rahul.shivaprasad@gmail.com>
2024-11-14 23:46:03 -0800

    Runtime: 0 ms (100.00%), Memory: 7.3 MB (100.00%) - Gitcode

--- 1041-robot-bounded-in-circle/1041. Robot Bounded In Circle.cpp
+++ 1041-robot-bounded-in-circle/1041. Robot Bounded In Circle.cpp
@@ -5,23 +5,21 @@ public:
         int currdir = 0;
         int currx = 0;
         int curry = 0;
-        for(int i = 0;i<4;i++){
-            for(auto x:instructions){
-                switch(x){
-                    case 'G':
-                        currx+=dirs[currdir][0];
-                        curry+=dirs[currdir][1];
-                        break;
-                    case 'L':
-                        currdir = (currdir-1+4)%4;
-                        break;
-                    case 'R':
-                        currdir = (currdir+1)%4;
-                        break;
-                }
+        for(auto x:instructions){
+            switch(x){
+                case 'G':
+                    currx+=dirs[currdir][0];
+                    curry+=dirs[currdir][1];
+                    break;
+                case 'L':
+                    currdir = (currdir-1+4)%4;
+                    break;
+                case 'R':
+                    currdir = (currdir+1)%4;
+                    break;
             }
-            if(currx==0 and curry==0)return true;
         }
-        return currx==0 and curry==0;
+
+        return currx==0 and curry==0 or currdir>0;
     }
 };
