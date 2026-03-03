43417190fb692ce9c226b52ad2a8ae4fe5a88435
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-03-02 21:44:20 -0800

    Runtime: 3 ms (57.18%), Memory: 16.4 MB (51.46%) - Gitcode

--- 253-meeting-rooms-ii/253. Meeting Rooms II.cpp
+++ 253-meeting-rooms-ii/253. Meeting Rooms II.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
         int n = intervals.size(), ans = 0;
-        vector<int> s,e;
+        vector<pair<int,int>> v;
         for(auto x:intervals){
-            s.push_back(x[0]);
-            e.push_back(x[1]);
+            v.push_back({x[0],1});
+            v.push_back({x[1],0});
         }
-        sort(s.begin(),s.end());
-        sort(e.begin(),e.end());
-        int i=0,j=0;
-        while(i<n){
-            while(i<n and s[i]<e[j]){
-                i++;
+        sort(v.begin(),v.end());
+        int curr = 0;
+        for(auto &x:v){
+            if(x.second){
+                curr++;
+                ans=max(curr,ans);
+            }else{
+                curr--;
             }
-            ans = max(ans,i-j);
-            j++;
         }
-        ans = max(ans,i-j);
         return ans;
     }
 };
