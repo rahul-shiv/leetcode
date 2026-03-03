6486295219f2ce781bbd3dc6ae291227832ab6f1
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-03-02 21:28:44 -0800

    Runtime: 15 ms (7.22%), Memory: 24 MB (21.95%) - Gitcode

--- 56-merge-intervals/56. Merge Intervals.cpp
+++ 56-merge-intervals/56. Merge Intervals.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
-        sort(intervals.begin(),intervals.end());
-        ans.emplace_back(intervals[0]);
-        for(int i = 1; i < intervals.size(); i++){
-            if(intervals[i][0]<=ans.back()[1]){
-                ans.back()[1]=max(intervals[i][1], ans.back()[1]);
+        for(auto interval:intervals){
+            if(ans.empty() or ans.back()[1]<interval[0]){
+                ans.push_back(interval);
             }else{
-                ans.emplace_back(intervals[i]);
+                ans.back()[1]=max(interval[1],ans.back()[1]);
             }
         }
         return ans;
