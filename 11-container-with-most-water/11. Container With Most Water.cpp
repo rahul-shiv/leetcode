cfa70d9772938add9a3c4e2c77ae66f333aad177
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-03-04 08:40:34 -0800

    Runtime: 52 ms (7.35%), Memory: 61.7 MB (99.94%) - Gitcode

--- 11-container-with-most-water/11. Container With Most Water.cpp
+++ 11-container-with-most-water/11. Container With Most Water.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
-        vector<int> l(n),r(n);
-        l[0]=height[0];
-        r[n-1]=height[n-1];
-        for(int i = 1; i < n; i++){
-            l[i]=max(l[i-1],height[i]);
-            r[n-i-1]=max(r[n-i],height[n-i-1]);
-        }
-        int i = 0, j = n-1, ans = min(l[0],r[n-1])*(n-1);
-        while(i<j){
-            if(l[i]>r[j]){
-                j--;
-            }else{
-                i++;
-            }
-            ans = max(ans,min(l[i],r[j])*(j-i));
+        int l = 0, r = height.size()-1, ans =0;
+        while(l<r){
+            ans = max(ans,min(height[l],height[r])*(r-l));
+            if(height[l]<height[r])l++;
+            else r--;
         }
         return ans;
     }
