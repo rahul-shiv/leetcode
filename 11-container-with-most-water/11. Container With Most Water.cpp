dab4c930468045b7c8bf9bda92a4d050ce15838a
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-03-09 21:23:46 -0700

    Runtime: 7 ms (7.35%), Memory: 62.8 MB (77.75%) - Gitcode

--- 11-container-with-most-water/11. Container With Most Water.cpp
+++ 11-container-with-most-water/11. Container With Most Water.cpp
@@ -5,12 +5,11 @@ public:
         int i = 0, j = n-1, ans = min(height[0],height[n-1])*(n-1);
         while(i<j){
             if(height[i]>height[j]){
-                ans = max(ans,height[j]*(j-i));
                 j--;
             }else{
-                ans = max(ans,height[i]*(j-i));
                 i++;
             }
+            ans = max(ans,min(height[i],height[j])*(j-i));
         }
         return ans;
     }
