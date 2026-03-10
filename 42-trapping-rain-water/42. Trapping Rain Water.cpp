e66540b4a27b3a129bf44c6cb32f3633fcf900f7
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-03-09 21:33:07 -0700

    Runtime: 4 ms (9.87%), Memory: 26 MB (78.51%) - Gitcode

--- 42-trapping-rain-water/42. Trapping Rain Water.cpp
+++ 42-trapping-rain-water/42. Trapping Rain Water.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int>lh(n),rh(n);
-        lh[0]=height[0];
-        rh[n-1]=height[n-1];
-        for(int i = 1; i<n;i++){
-            lh[i]=max(lh[i-1],height[i]);
-            rh[n-i-1]=max(rh[n-i],height[n-i-1]);
-        }
-        int ans = 0;
-        for(int i = 0;i<n;i++){
-            ans += min(lh[i],rh[i])-height[i];
+        int l = 0, r = height.size()-1, ans =0, lh =0, rh=0, t;
+        while(l<=r){
+            if (lh<rh){
+                ans += max(0, min(lh,rh)-height[l]);
+                lh = max(lh,height[l]);
+                l++;
+            }else{
+                ans += max(0, min(lh,rh)-height[r]);
+                rh = max(rh,height[r]);
+                r--;
+            }
         }
         return ans;
     }
-    // redo with stack
 };
