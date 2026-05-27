fc9f4cdae7bd8432e64975aa1e311ebc2d32d451
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-05-26 18:27:00 -0700

    Runtime: 32 ms (20.23%), Memory: 27.1 MB (25.49%) - Gitcode

--- 49-group-anagrams/49. Group Anagrams.cpp
+++ 49-group-anagrams/49. Group Anagrams.cpp
@@ -1,15 +1,20 @@
 class Solution {
+    vector<int> to_vec(string &s){
+        vector<int> vec(26);
+        for(auto c:s){
+            vec[c-'a']++;
+        }
+        return vec;
+    }
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>> m;
-        for(auto s:strs){
-            auto x = s;
-            sort(x.begin(),x.end());
-            m[x].push_back(s);
+        map<vector<int>, vector<string>> m;
+        for(auto str:strs){
+           m[to_vec(str)].push_back(str);
         }
         vector<vector<string>> ans;
-        for(auto x:m){
-            ans.push_back(x.second);
+        for(auto &x:m){
+            ans.emplace_back(vector<string>(x.second.begin(),x.second.end()));
         }
         return ans;
     }
