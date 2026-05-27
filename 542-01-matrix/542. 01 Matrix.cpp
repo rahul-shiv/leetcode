ad2ad0798bc2361acd6b7b2b82d7c9d0acde0451
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-05-26 19:22:11 -0700

    Runtime: 119 ms (5.63%), Memory: 60.3 MB (5.57%) - Gitcode

--- 542-01-matrix/542. 01 Matrix.cpp
+++ 542-01-matrix/542. 01 Matrix.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size(),x,y,nx,ny;
-        int dirs[][2] = {{0,1},{1,0},{0,-1},{-1,0}};
-        vector<vector<int>> ans(m,vector<int>(n,-1));
-        queue<pair<int,int>> q;
-        for(int i = 0; i<m;i++){
-            for(int j = 0; j<n;j++){
-                if(!mat[i][j]){
-                    ans[i][j]=0;
-                    q.push({i,j});
-                }
+        int m = mat.size(), n = mat[0].size();
+        queue<vector<int>> q;
+        vector<vector<int>> ans(m, vector<int>(n));
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j]==0)q.push({i,j});
             }
         }
+        int dirs[][2]={{-1,0},{1,0},{0,-1},{0,1}};
         while(!q.empty()){
-            tie(x,y)=q.front();
+            vector<int> curr = q.front();
             q.pop();
             for(auto dir:dirs){
-                nx = x+dir[0];
-                ny = y+dir[1];
-                if(nx>=0 and nx<m and ny>=0 and ny<n and ans[nx][ny]==-1){
-                    ans[nx][ny]=ans[x][y]+1;
-                    q.push({nx,ny});
+                int x = curr[0]+dir[0], y = curr[1]+dir[1];
+                if(x>=0 and x<m and y>=0 and y<n and mat[x][y]!=0 and ans[x][y]==0){
+                    q.push({x,y});
+                    ans[x][y]=ans[curr[0]][curr[1]]+1;
                 }
             }
         }
