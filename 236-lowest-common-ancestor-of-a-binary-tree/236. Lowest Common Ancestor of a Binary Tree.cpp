09c98f1e6fae8c8d19c55fa2ba323c548bfc0268
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-06-17 08:55:14 -0700

    Runtime: 67 ms (11.67%), Memory: 44 MB (12.75%) - Gitcode

--- 236-lowest-common-ancestor-of-a-binary-tree/236. Lowest Common Ancestor of a Binary Tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -10,12 +10,11 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==p || root==q || !root)return root;
-        TreeNode *l = nullptr, *r = nullptr;
-        l = lowestCommonAncestor(root->left,p,q);
-        r = lowestCommonAncestor(root->right,p,q);
-        if(l and r)return root;
+        if(p==root||q==root || !root)return root;
+        TreeNode* l = lowestCommonAncestor(root->left,p,q);
+        TreeNode* r = lowestCommonAncestor(root->right,p,q);
+        if(l and r) return root;
         if(l) return l;
-        else return r;
+        return r;
     }
 };
