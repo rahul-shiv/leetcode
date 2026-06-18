d61b26b68bb535644e50c0c352b7e1b2cb5b178e
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-06-17 19:42:21 -0700

    Runtime: 8 ms (55.26%), Memory: 10.2 MB (99.99%) - Gitcode

--- 76-minimum-window-substring/76. Minimum Window Substring.cpp
+++ 76-minimum-window-substring/76. Minimum Window Substring.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int>m(128,INT_MAX);
-        int notvalid=0;
+        vector<int> tv(128),sv(128);
+        int n = s.length(), count=0;
         for(auto c:t){
-            if(m[c]==INT_MAX)m[c]=1;
-            else m[c]++;
-            if(m[c]==1)notvalid++;
+            if(!tv[c-'A'])count++;
+            tv[c-'A']+=1;
         }
-        deque<int> q;
-        int cnt = t.length();
-        string ans=s;
-        int i=0,l=0,r=INT_MAX-2;
-        while(i<s.length()){
-            // find next letter
-            while(i<s.length()){
-                i++;
-                if(m[s[i-1]]!=INT_MAX){
-                    q.push_back(i-1);
-                    m[s[i-1]]--;
-                    if(m[s[i-1]]==0)notvalid--;
-                    break;
+        int l=0,r=0,minl=INT_MAX,start=-1;
+        while(r<n){
+            while(r<n and count){
+                sv[s[r]-'A']++;
+                if(tv[s[r]-'A'] and tv[s[r]-'A']==sv[s[r]-'A']) count--;
+                r++;
+            }
+            while(l<r and count==0){
+                if(r-l<minl){
+                    minl=r-l;
+                    start=l;
                 }
-            }
-            // squeeze till still valid
-            while(!notvalid and m[s[q.front()]]<0){
-                m[s[q.front()]]++;
-                q.pop_front();
-            }
-            // save
-            if(!notvalid and (r-l+1)>(i-q.front())){
-                r=i;
-                l=q.front();
+                sv[s[l]-'A']--;
+                if(tv[s[l]-'A'] and tv[s[l]-'A']>sv[s[l]-'A']) count++;
+                l++;
             }
         }
-        return notvalid?"":s.substr(l,r-l);
+        return minl==INT_MAX?"":s.substr(start,minl);
     }
 };
