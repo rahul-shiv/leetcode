76d9d19b5282e46193a03e5a4867e0dc010afd81
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-06-22 07:47:40 -0700

    Runtime: 100 ms (8.59%), Memory: 32.2 MB (99.99%) - Gitcode

---
+++ 721-accounts-merge/721. Accounts Merge.cpp
@@ -0,0 +1,58 @@
+class Solution {
+    vector<int> id,sz;
+    int find(int x){
+        while(x!=id[x]){
+            id[x]=id[id[x]];
+            x = id[x];
+        }
+        return id[x];
+    }
+    void uni(int x, int y){
+        int a = find(x);
+        int b = find(y);
+        if(a!=b){
+            if(sz[a]<sz[b]){
+                swap(a,b);
+                swap(x,y);
+            }
+            sz[a]+=sz[b];
+            id[b]=id[a];
+        }
+    }
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        map<string,int> a2i;
+        
+        int n = accounts.size();
+        id = vector<int> (n);
+        sz = vector<int> (n,1);
+        for(int i = 0; i < n; i++) id[i]=i;
+
+        int ind = 0;
+        for(auto account:accounts){
+            for(int i = 1; i < account.size(); i++){
+                auto it = a2i.find(account[i]);
+                if(it==a2i.end()){
+                    a2i[account[i]] = ind;
+                }else{
+                    uni(it->second,ind);
+                }
+            }
+            ind++;
+        }
+
+        vector<vector<string>> ans;
+        vector<int> i2a(n,-1);
+        int pos;
+        for(auto a2i_it:a2i){
+            pos = find(a2i_it.second);
+            if(i2a[pos]==-1){
+                i2a[pos] = ans.size();
+                ans.push_back({accounts[pos][0]});
+                
+            }
+            ans[i2a[pos]].push_back(a2i_it.first);
+        }
+        return ans;
+    }
+};
