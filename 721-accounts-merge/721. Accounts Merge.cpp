f6aa97645043b6e77bb0035783e12fe09a038bcc
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-06-22 07:57:15 -0700

    Runtime: 105 ms (7.67%), Memory: 32.2 MB (99.99%) - Gitcode

--- 721-accounts-merge/721. Accounts Merge.cpp
+++ 721-accounts-merge/721. Accounts Merge.cpp
@@ -1,58 +1,60 @@
 class Solution {
-    int n;
-    vector<int> id;
-    vector<int> size;
-    map<string,int> a2i;
-    vector<string> names;
-public:
+    vector<int> id,sz;
     int find(int x){
         while(x!=id[x]){
             id[x]=id[id[x]];
             x = id[x];
         }
-        return x;
+        return id[x];
     }
-    int merge(int a, int b){
-        int x = find(a);
-        int y = find(b);
-        if(x!=y){
-            if(size[y]>size[x])swap(x,y);
-            size[x]+=size[y];
-            id[y]=x;
-        }
-        return x;
-    }
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        n = accounts.size();
-        id.resize(n);
-        size.resize(n,1);
-        for(int i=0;i<n;i++)id[i]=i;
-        for(auto account:accounts){
-            names.push_back(account[0]);
-            int j = names.size()-1;
-            int x = find(j);
-            for(int i =1;i<account.size();i++){
-                auto it = a2i.find(account[i]);
-                if(it!=a2i.end()){
-                    x = merge(x,find(it->second));
-                    id[j] = x;
-                }
-                a2i[account[i]] = x;
+    void uni(int x, int y){
+        int a = find(x);
+        int b = find(y);
+        if(a!=b){
+            if(sz[a]<sz[b]){
+                swap(a,b);
+                swap(x,y);
             }
+            sz[a]+=sz[b];
+            id[b]=id[a];
+            find(y);
         }
-        map<int,vector<string>> i2s;
-        for(auto a:a2i){
-            int x = find(a.second);
-            i2s[x].push_back(a.first);
+    }
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        map<string,int> a2i;
+        
+        int n = accounts.size();
+        id = vector<int> (n);
+        sz = vector<int> (n,1);
+        for(int i = 0; i < n; i++) id[i]=i;
+
+        int ind = 0;
+        for(auto account:accounts){
+            for(int i = 1; i < account.size(); i++){
+                auto it = a2i.find(account[i]);
+                if(it==a2i.end()){
+                    a2i[account[i]] = ind;
+                }else{
+                    uni(it->second,ind);
+                }
+            }
+            ind++;
         }
+        for(int i = 0; i < n; i++) find(i);
+
         vector<vector<string>> ans;
-        for(auto x:i2s){
-            vector<string> temp;
-            temp.push_back(names[x.first]);
-            for(auto y:x.second) temp.push_back(y);
-            ans.push_back(temp);
+        vector<int> i2a(n,-1);
+        int pos;
+        for(auto a2i_it:a2i){
+            pos = find(a2i_it.second);
+            if(i2a[pos]==-1){
+                i2a[pos] = ans.size();
+                ans.push_back({accounts[pos][0]});
+                
+            }
+            ans[i2a[pos]].push_back(a2i_it.first);
         }
         return ans;
-        
     }
 };
