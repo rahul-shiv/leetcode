ed3eb180b89a45295f4d0b58eb9bdb97499bcd3b
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-06-24 19:18:01 -0700

    Runtime: 59 ms (24.11%), Memory: 62.4 MB (19.72%) - Gitcode

--- 310-minimum-height-trees/310. Minimum Height Trees.cpp
+++ 310-minimum-height-trees/310. Minimum Height Trees.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        vector<int> ans, indegree(n);
+        vector<int> indegree(n);
         vector<vector<int>> g(n);
         for(auto edge:edges){
-            g[edge[0]].push_back(edge[1]);
-            g[edge[1]].push_back(edge[0]);
             indegree[edge[0]]++;
             indegree[edge[1]]++;
+            g[edge[0]].push_back(edge[1]);
+            g[edge[1]].push_back(edge[0]);
         }
         queue<pair<int,int>> q;
-        int l = 0;
-        int u,cl;
-        for(int i = 0; i < n; i++){
-            if(indegree[i]==1||indegree[i]==0){
-                q.push({i,0});
-                indegree[i]--;
-            }
+        vector<int> ans;
+        int currlevel = 0;
+        for (int i = 0; i<n; i++){
+            if(indegree[i]==1)q.push(make_pair(i,0));
         }
+        if(q.size()==0)return {0};
+        int node, level;
         while(!q.empty()){
-            tie(u,cl) = q.front();
+            tie(node,level) = q.front();
             q.pop();
-            if(cl==l){
-                ans.push_back(u);
-            }else{
-                l=cl;
-                ans = vector<int>();
-                ans.push_back(u);
+            if(level>currlevel){
+                ans={};
+                currlevel=level;
             }
-            for(auto v:g[u]){
-                indegree[v]--;
-                if(indegree[v]==1){
-                    q.push({v,cl+1});
+            ans.push_back(node);
+            for(auto next:g[node]){
+                indegree[next]--;
+                if(indegree[next]==1){
+                    q.push(make_pair(next,level+1));
                 }
             }
+
         }
         return ans;
     }
