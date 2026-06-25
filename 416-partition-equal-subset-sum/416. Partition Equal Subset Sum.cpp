ff883ac7979df1194377c9f6d9c88fe4f20b49d2
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-06-24 18:50:44 -0700

    Runtime: 27 ms (97.67%), Memory: 13.4 MB (85.65%) - Gitcode

--- 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.cpp
+++ 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.cpp
@@ -4,13 +4,14 @@ public:
         int s = accumulate(nums.begin(),nums.end(),0);
         if(s%2)return false;
         s/=2;
-        set<int> ans;
-        ans.insert(0);
+        vector<bool> ans(s+1);
+        ans[0]=1;
         sort(nums.begin(),nums.end());
         for(int i = 0; i < nums.size();i++){
-            for(auto x=ans.rbegin();x!=ans.rend();x++){
-                if(*x+nums[i]==s)return true;
-                ans.insert(*x+nums[i]);
+            for(int j = s;j>=0;j--){
+                if(ans[j]==0 or j+nums[i]>s)continue;
+                if(j+nums[i]==s)return true;
+                ans[j+nums[i]]=true;
             }
         }
         return false;
