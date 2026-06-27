001ea8a3a82a3d2bf06f405aaf2bfd0de40ce153
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-06-27 10:21:18 -0700

    Runtime: 65 ms (22.05%), Memory: 94.3 MB (20.26%) - Gitcode

--- 435-non-overlapping-intervals/435. Non-overlapping Intervals.cpp
+++ 435-non-overlapping-intervals/435. Non-overlapping Intervals.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end(),[](const auto &a, const auto &b){
-            return a[1]==b[1]?a[0]>b[0]:a[1]<b[1];
+            return a[1]==b[1]?a[0]<b[0]:a[1]<b[1];
         });
-        int ans = 0, prev = intervals[0][1];
-        for(int i = 1; i < intervals.size(); i++){
-            if(intervals[i][0]<prev){
-                ans++;
-            }else{
-                prev = intervals[i][1];
+        int n = intervals.size();
+        vector<vector<int>> sol;
+        for(int i = 0; i < n; i++){
+            if(sol.empty() or intervals[i][0]>=sol.back()[1]){
+                sol.push_back(intervals[i]);
+            }else if(intervals[i][1]<sol.back()[1]){
+                sol.back()=intervals[i];
             }
         }
-        return ans;
+        return n-sol.size();
     }
 };
