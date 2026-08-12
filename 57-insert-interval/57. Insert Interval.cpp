d744c217cbeb27f38d5fad87c649073101fa629f
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-08-11 18:57:34 -0700

    Runtime: 0 ms (100.00%), Memory: 21.6 MB (95.61%) - Gitcode

--- 57-insert-interval/57. Insert Interval.cpp
+++ 57-insert-interval/57. Insert Interval.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size();
         vector<vector<int>> ans;
-        if(intervals.empty()){
-            ans.push_back(newInterval);
-            return ans;
+        int i = 0;
+        while(i < n && intervals[i][1] <  newInterval[0]) ans.push_back(intervals[i++]);
+        while(i < n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
         }
-        for(int i = 0; i < intervals.size();){
-            vector<int> chosen(2);
-            if(newInterval[0]!=-1 and newInterval[0]<=intervals[i][0]){
-                chosen[0]=newInterval[0];
-                chosen[1]=newInterval[1];
-                newInterval[0]=-1;
-            }else{
-                chosen = intervals[i];
-                i++;
-            }
-            if(ans.empty()||chosen[0]>ans.back()[1]){
-                ans.push_back(chosen);
-            }else{
-                ans.back()[1]=max(ans.back()[1],chosen[1]);
-            }
-        }
-        if(newInterval[0]!=-1){
-            if(newInterval[0]>ans.back()[1])ans.push_back(newInterval);
-            else ans.back()[1]=max(ans.back()[1],newInterval[1]);
-        }
+        ans.push_back(newInterval);
+        while(i < n) ans.push_back(intervals[i++]);
+
         return ans;
     }
 };
