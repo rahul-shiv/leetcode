ca7198de324c42998abffa29a2d9623be8de51a8
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-08-13 08:09:42 -0700

    Runtime: 31 ms (23.77%), Memory: 19.3 MB (16.44%) - Gitcode

--- 3-longest-substring-without-repeating-characters/3. Longest Substring Without Repeating Characters.cpp
+++ 3-longest-substring-without-repeating-characters/3. Longest Substring Without Repeating Characters.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         unordered_map<char,int> m;
-        int ans = 0, start = -1;
+        int ans = 0, prev = -1;
         for(int i = 0; i < s.length(); i++){
             auto it = m.find(s[i]);
-            if(it==m.end()){
-                m[s[i]]=i;
+            if(it==m.end() or it->second<=prev){
+                ans = max(ans,i-prev);
             }else{
-                start = max(start,m[s[i]]);
-                m[s[i]] = i;
+                prev = m[s[i]];
             }
-            ans = max(ans,i-start);
+            m[s[i]]=i;
         }
         return ans;
     }
