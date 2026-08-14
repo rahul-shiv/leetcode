6a495696e07fcf348a2727f345af0574afba1633
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-08-14 08:12:41 -0700

    Runtime: 0 ms (100.00%), Memory: 112.5 MB (26.39%) - Gitcode

--- 134-gas-station/134. Gas Station.cpp
+++ 134-gas-station/134. Gas Station.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n = gas.size(),g = gas[0],s=0;
-        for(int i = 1; i < 2*n;i++){
-            g = g - cost[(i-1)%n];
-            if(g<0){
-                s = i;
-                g = 0;
+        int n = gas.size(), curr =0, s = 0, t=0;
+        for(int i = 0;i<n;i++){
+            s+=gas[i]-cost[i];
+            t+=gas[i]-cost[i];
+            if(s<0){
+                curr=i+1;
+                s = 0;
             }
-            if(i-s==n)return s;
-            g+=gas[i%n];
         }
-        return -1;
+        return t>=0?curr:-1;
     }
 };
