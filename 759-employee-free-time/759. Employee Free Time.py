69b543174159021bc195276be1e2397875c1622b
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-08-14 18:52:28 -0700

    Runtime: 1968 ms (5.22%), Memory: 16.1 MB (100.00%) - Gitcode

---
+++ 759-employee-free-time/759. Employee Free Time.py
@@ -0,0 +1,38 @@
+"""
+# Definition for an Interval.
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        def findFree(sched):
+            ret = [Interval(float('-inf'),sched[0].start)]
+            for i in range(1,len(sched)):
+                ret.append(Interval(sched[i-1].end,sched[i].start))
+            ret.append(Interval(sched[-1].end,float('inf')))
+            return ret
+        freeSchedule = []
+        for sched in schedule:
+            freeSchedule.append(findFree(sched))
+        def merge(x,y):
+            ret = []
+            for i in x:
+                for j in y:
+                    if i.end<j.start or i.start>j.end :
+                        continue
+                    else:
+                        ret.append(Interval(max(i.start,j.start),min(i.end,j.end)))
+            return ret
+        ans = freeSchedule[0]
+        for i in range(1,len(freeSchedule)):
+            ans = merge(ans,freeSchedule[i])
+        ans = ans[1:-1]
+        ans2 = []
+        for x in ans:
+            if x.start<x.end:
+                ans2.append(x)
+        return ans2
+                
