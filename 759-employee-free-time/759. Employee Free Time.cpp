1cc91742cf37a0f0f30d98275cd04913aa9ab842
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-08-14 18:53:26 -0700

    Runtime: 38 ms (5.40%), Memory: 20.2 MB (7.95%) - Gitcode

--- 759-employee-free-time/759. Employee Free Time.cpp
+++ 759-employee-free-time/759. Employee Free Time.cpp
@@ -17,25 +17,20 @@ public:
 class Solution {
 public:
     vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
-        int prev = -1;
-        vector<Interval> ans;
-        int c=0;
-        vector<int> starts;
-        vector<int> ends;
-
-        for(int i = 0; i<schedule.size();i++){
-            for(auto s:schedule[i]){
-                starts.push_back(s.start);
-                ends.push_back(s.end);
+        vector<vector<int>> flat;
+        for(auto x: schedule){
+            for(auto k:x){
+                flat.push_back({k.start,k.end});
             }
         }
-        sort(starts.begin(),starts.end());
-        sort(ends.begin(),ends.end());
-        int s = 0, e = 0, n = starts.size();
-        while(s<n){
-            while(ends[e]<starts[s]){prev=ends[e];e++;}
-            if(e==s and prev!=-1) ans.push_back(Interval(prev,starts[s]));
-            s++;
+        sort(flat.begin(),flat.end());
+        vector<Interval> ans;
+        int prev = flat[0][0];
+        for(auto x:flat){
+            if(prev<x[0]){
+                ans.push_back(Interval(prev,x[0]));
+            }
+            prev=max(prev,x[1]);
         }
         return ans;
     }
