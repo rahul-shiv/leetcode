78b3d5db4ca988a0ecc16fc9f4892478f1a70c7c
rahul-shiv <rahul.shivaprasad@gmail.com>
2026-08-16 15:34:44 -0700

    Runtime: 30 ms (7.29%), Memory: 13.6 MB (100.00%) - Gitcode

--- 787-cheapest-flights-within-k-stops/787. Cheapest Flights Within K Stops.cpp
+++ 787-cheapest-flights-within-k-stops/787. Cheapest Flights Within K Stops.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int,int>> ans(n,{INT_MAX,INT_MAX});
-        vector<vector<pair<int,int>>> g(n);
+        vector<vector<pair<int,int>>> adj(n);
         for(auto flight:flights){
-            g[flight[0]].push_back({flight[1],flight[2]});
+            adj[flight[0]].push_back({flight[1],flight[2]});
         }
-        priority_queue<tuple<int,int,int>> pq;
-        ans[src]={0,0};
-        pq.push({0,-1,src});
-        int u,hops,dist,v,vdist;
-        while(!pq.empty()){
-            tie(dist,hops,u)=pq.top();
-            pq.pop();
-            for(auto &vinfo:g[u]){
-                tie(v,vdist)=vinfo;
-                if(hops+1<=k and (hops+1<ans[v].second or dist+vdist<ans[v].first)){
-                    ans[v].first=min(dist+vdist,ans[v].first);
-                    ans[v].second=min(hops+1,ans[v].second);
-                    pq.push({dist+vdist,hops+1,v});
+        queue<tuple<int,int,int>> q;
+        vector<int> dist(n,INT_MAX);
+        q.push({src,0,0});
+        dist[src] = 0;
+        int u,l,d;
+        while(!q.empty()){
+            tie(u,l,d) = q.front();
+            q.pop();
+            if(l<=k){
+                for(auto v:adj[u]){
+                    if(d+v.second<dist[v.first]){
+                        dist[v.first] = d+v.second;
+                        q.push({v.first,l+1,dist[v.first]});
+                    }
                 }
             }
         }
-        return ans[dst].first==INT_MAX?-1:ans[dst].first;
+        return dist[dst]==INT_MAX?-1:dist[dst];
     }
 };
